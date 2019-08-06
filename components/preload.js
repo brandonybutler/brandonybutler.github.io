8e7ac0f61aa0bd1fe8c96beaf38d47ede53a29dd
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-06 18:45:56 +1000

    Corrected use of listeners.

--- components/preload.js
+++ components/preload.js
@@ -1,11 +1,9 @@
-$(document).ready(() => {
-  setTimeout(() => {
-    $("body").css({
-      "animation": "blurin 2s",
-      "-webkit-animation": "blurin 2s",
-      "-moz-animation": "blurin 2s",
-      "-ms-animation": "blurin 2s",
-    });
-    $("body").show();
-  }, 1500);
+$(window).load(() => {
+  $("body").css({
+    "animation": "blurin 2s",
+    "-webkit-animation": "blurin 2s",
+    "-moz-animation": "blurin 2s",
+    "-ms-animation": "blurin 2s",
+  });
+  $("body").show();
 });
