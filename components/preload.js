39cc2e1e9df4c234ceeb7fd881a47e92e5c10ed6
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-06 18:16:15 +1000

    Added blurin animation.

--- components/preload.js
+++ components/preload.js
@@ -5,3 +5,12 @@ function preload(imageArray) {
 }
 
 preload(["../resources/bubble.jpg"]);
+
+$(() => {
+  $("body").css({
+    "animation": "blurin 2s",
+    "-webkit-animation": "blurin 2s",
+    "-moz-animation": "blurin 2s",
+    "-ms-animation": "blurin 2s",
+  });
+});
