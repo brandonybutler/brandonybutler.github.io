55b6a62866bf39e6c2add240096f999256cc01ea
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-06 18:25:14 +1000

    Created preload script.

---
+++ components/preload.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $("body").css({
+    "animation": "blurin 2s",
+    "-webkit-animation": "blurin 2s",
+    "-moz-animation": "blurin 2s",
+    "-ms-animation": "blurin 2s",
+  });
+  $("body").show();
+});
