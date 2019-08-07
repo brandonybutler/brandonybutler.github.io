ba4f9f54e08ddab09afa25ab43d91db7ff85a46c
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-07 17:59:24 +1000

    Added clearInterval once image is completed loading

--- components/preload.js
+++ components/preload.js
@@ -1,17 +1,23 @@
-$(window).on("load", () => {
-    console.log("Checking if background image is loaded!");
-  
-    setTimeout(() => {
-      if (($("<img />").complete) && (!$("<img />").naturalWidth === 0)) {
-          console.log("Background image is loaded!");
-        
-          $("body").css({
-            "animation": "blurin 2s",
-            "-webkit-animation": "blurin 2s",
-            "-moz-animation": "blurin 2s",
-            "-ms-animation": "blurin 2s",
-          });
-          $("body").show();
-      }
-    }, 1000);
-  });
+var interval = setInterval(loader, 1000);
+
+function loader() {
+    $(window).on("load", () => {
+        console.log("Checking if background image is loaded!");
+
+        setInterval(() => {
+          if (($("<img />").complete) && (!$("<img />").naturalWidth === 0)) {
+              console.log("Background image is loaded!");
+
+              $("body").css({
+                "animation": "blurin 2s",
+                "-webkit-animation": "blurin 2s",
+                "-moz-animation": "blurin 2s",
+                "-ms-animation": "blurin 2s",
+              });
+              $("body").show();
+              
+              clearInterval(interval);
+          }
+        }, 1000);
+    });
+}
