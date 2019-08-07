e6333f9b8b6902f171e43c4a621e4ddb3d257db6
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-07 17:24:27 +1000

    Added a fallback method if the image fails to load

--- components/preload.js
+++ components/preload.js
@@ -1,9 +1,21 @@
-$(window).on("load", () => {
-  $("body").css({
-    "animation": "blurin 2s",
-    "-webkit-animation": "blurin 2s",
-    "-moz-animation": "blurin 2s",
-    "-ms-animation": "blurin 2s",
+$("<img />")
+  .on("load", () => {
+    $("body").css({
+      "animation": "blurin 2s",
+      "-webkit-animation": "blurin 2s",
+      "-moz-animation": "blurin 2s",
+      "-ms-animation": "blurin 2s",
+    });
+    $("body").show();
+  })
+  .on("error", () => {
+    if (($("<img />").complete) || (!$("<img />").naturalWidth === 0) {
+        $("body").css({
+          "animation": "blurin 2s",
+          "-webkit-animation": "blurin 2s",
+          "-moz-animation": "blurin 2s",
+          "-ms-animation": "blurin 2s",
+        });
+        $("body").show();
+    }
   });
-  $("body").show();
-});
