d91dc1c4842ecd8043230d64f9b2d62a8077f440
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-07 18:04:27 +1000

    Testing.

--- components/preload.js
+++ components/preload.js
@@ -1,23 +1,20 @@
-var interval = setInterval(loader, 1000);
-
-function loader() {
-    $(window).on("load", () => {
-        console.log("Checking if background image is loaded!");
-
-        setInterval(() => {
-          if (($("<img />").complete) && (!$("<img />").naturalWidth === 0)) {
-              console.log("Background image is loaded!");
-
-              $("body").css({
-                "animation": "blurin 2s",
-                "-webkit-animation": "blurin 2s",
-                "-moz-animation": "blurin 2s",
-                "-ms-animation": "blurin 2s",
-              });
-              $("body").show();
-              
-              clearInterval(interval);
-          }
-        }, 1000);
-    });
-}
+$(window).on("load", () => {
+    console.log("Checking if background image is loaded!");
+    var interval = setInterval(this, 1000);
+        
+    setInterval(() => {
+      if (($("<img />").complete) && (!$("<img />").naturalWidth === 0)) {
+          console.log("Background image is loaded!");
+          
+          $("body").css({
+            "animation": "blurin 2s",
+            "-webkit-animation": "blurin 2s",
+            "-moz-animation": "blurin 2s",
+            "-ms-animation": "blurin 2s",
+          });
+          $("body").show();
+          
+          clearInterval(interval);
+      }
+    }, 1000);
+});
