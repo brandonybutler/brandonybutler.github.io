998aae4213e7db5e10355a09b368e1b16aa98dd4
Brandon <49143762+brandonybutler@users.noreply.github.com>
2019-08-07 17:40:56 +1000

    Testing.

--- components/preload.js
+++ components/preload.js
@@ -1,21 +1,10 @@
-$("<img />")
-  .on("load", () => {
-    console.log("Background image is loaded!");
-  
-    $("body").css({
-      "animation": "blurin 2s",
-      "-webkit-animation": "blurin 2s",
-      "-moz-animation": "blurin 2s",
-      "-ms-animation": "blurin 2s",
-    });
-    $("body").show();
-  })
-  .on("error", () => {
-    console.log("An error occurred in the initial load loop.");
+$(window).on("load", () => {
+    console.log("Checking if background image is loaded!");
   
     while (true) {
       if (($("<img />").complete) || (!$("<img />").naturalWidth === 0)) {
           console.log("Background image is loaded!");
+        
           $("body").css({
             "animation": "blurin 2s",
             "-webkit-animation": "blurin 2s",
@@ -23,8 +12,7 @@ $("<img />")
             "-ms-animation": "blurin 2s",
           });
           $("body").show();
-      } else {
-        console.log("Background image is not completed loading!");
+          break;
       }
     }
   });
